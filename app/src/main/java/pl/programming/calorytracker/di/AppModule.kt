f88c1548e8941502c7fb2a74ac5fc5f078fa1897
Piotr Rudzki <pietrekr@wp.pl>
2023-11-29 09:38:36 +0100

    Preferences setup com.squareup:javapoet:1.13 dependency was added to fix building error

---
+++ app/src/main/java/pl/programming/calorytracker/di/AppModule.kt
@@ -0,0 +1,31 @@
+package pl.programming.calorytracker.di
+
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import pl.programming.core.domain.DefaultPreferences
+import pl.programming.core.domain.preferences.Preferences
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(
+        app: Application,
+    ): SharedPreferences {
+        return app.getSharedPreferences("shared_pref", MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun providePreferences(sharedPreferences: SharedPreferences): Preferences {
+        return DefaultPreferences(sharedPreferences)
+    }
+}
